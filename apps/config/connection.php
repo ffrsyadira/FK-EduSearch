97ef52526958259a3c215eb3383ed9c756b4e3ce
ffrsyadira <135602778+ffrsyadira@users.noreply.github.com>
2023-06-21 10:32:24 +0800

    connection.php

--- apps/config/connection.php
+++ apps/config/connection.php
@@ -1,13 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "fk-edusearch";
+$servername = "localhost"; // Replace with your server name
+$username = "your_username"; // Replace with your database username
+$password = "your_password"; // Replace with your database password
+$database = "your_database"; // Replace with your database name
 
-try {
-	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo("<script>console.log('connection on')</script>");
-} catch (PDOException $e) {
-	echo $e->getMessage();
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+// Perform database operations here
+
+// Close the connection
+$conn->close();
+?>
